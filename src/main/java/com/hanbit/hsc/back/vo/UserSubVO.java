32878cb89407247cb1ef0140b4bdbc08eab4a51e
nahcoos <hscsc515@gmail.com>
2017-08-07 17:21:21 +0900

    update user-page visited country icon

---
+++ src/main/java/com/hanbit/hsc/back/vo/UserSubVO.java
@@ -0,0 +1,51 @@
+package com.hanbit.hsc.back.vo;
+
+public class UserSubVO {
+
+	private String visited;
+	private String visitedImg;
+	private int userNo;
+	private String userId;
+	private String usersImg;
+
+	public String getVisited() {
+		return visited;
+	}
+
+	public void setVisited(String visited) {
+		this.visited = visited;
+	}
+
+	public String getVisitedImg() {
+		return visitedImg;
+	}
+
+	public void setVisitedImg(String visitedImg) {
+		this.visitedImg = visitedImg;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUsersImg() {
+		return usersImg;
+	}
+
+	public void setUsersImg(String usersImg) {
+		this.usersImg = usersImg;
+	}
+
+}
